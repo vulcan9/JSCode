2faa08cf100e19ed3d5f10c20e95b838785cb5df
vulcan9-Home <pdi1066@naver.com>
2013-10-12 18:06:59 +0900

    .metadata 제거
    Signed-off-by: vulcan9-Home <pdi1066@naver.com>

---
+++ dropinrequireJS/myApp.js
@@ -0,0 +1,15 @@
+var myApp = {};
+
+myApp.application = {
+	template : '<div>\
+				<h1>This is Hello World Example</h1>\
+			</div>',
+	start : function()
+	{
+		console.log("module loaded");
+	},
+	end : function()
+	{
+		console.log("module un-loaded");
+	}
+}
